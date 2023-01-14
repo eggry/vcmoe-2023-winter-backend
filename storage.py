3a9c09d4044afaa3f19297852462da4b19467090
Eggry Ran <eggry@live.cn>
2023-01-14 22:31:40 +0800

    change dump format

--- storage.py
+++ storage.py
@@ -1,5 +1,5 @@
 import json
-from datetime import datetime
+from datetime import datetime, timedelta,timezone
 import os
 
 __data_dir = os.getenv("VC_MOE_DATA_DIR", "./data")
@@ -11,6 +11,6 @@ print(f"Data dir: {__data_dir}")
 
 
 def dump_to_json(tag, data):
-    timecode = datetime.now().strftime('%Y%m%d-%H%M%S-%f')
-    with open(f"{__data_dir}/dump-{tag}-{timecode}.json", "w", encoding="utf-8") as f:
+    timecode = datetime.now(timezone(timedelta(hours=8))).strftime('%Y%m%d-%H%M%S-%f')
+    with open(f"{__data_dir}/{timecode}-{tag}-dump.json", "w", encoding="utf-8") as f:
         json.dump(data, f)
