bc836410a1fcbd1f6bf6f964512e99c53918817e
Eggry Ran <eggry@live.cn>
2023-02-03 17:58:26 +0800

    refine election format

--- src/election.py
+++ src/election.py
@@ -5,12 +5,12 @@ import re
 def parse_election(reply):
     if reply["mid"] == HOST_UID:
         if res := re.match(
-            r"^【(.*)】(\d+)进(\d+)", reply["content"]["message"]
+            r"^【(.*)】(.*)", reply["content"]["message"]
         ):
             title = res[0]
-            group = res[1]
-            count = res[2]
-            final = res[3]
+            # group = res[1]
+            # count = res[2]
+            # final = res[3]
             videos = re.findall(r"([0-9A-Z]+)[\. ](.*av[\d/]*|.*BV[A-Za-z0-9/]*)",
                                 reply["content"]["message"])
             jump_url = reply["content"]["jump_url"]
@@ -20,9 +20,9 @@ def parse_election(reply):
             return ({
                 "rpid": reply["rpid"],
                 "title": title,
-                "group": group,
-                "count": count,
-                "final": final,
+                # "group": group,
+                # "count": count,
+                # "final": final,
                 "videos": videos,
             })
     return None
